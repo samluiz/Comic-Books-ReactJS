1f03f042208235fe8b02ecdfaa7153e799acc603
samluiz <samuelluizrl@outlook.com>
2022-07-31 12:28:55 -0300

    Fix deploy error

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App
-            showModal={function (): void {
-                throw new Error('Function not implemented.')
-            }}
-            isModalActive={false}
-            itemsPerPage={0}
-            totalItems={0}
-            comics={[]}
-        />
+        <App />
     </React.StrictMode>
 )
 
